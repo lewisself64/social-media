0d3cf09f77e50dd2d654b89f5c8e8329abebf940
lewisself64 <lewisself64@gmail.com>
2018-04-16 08:45:26 +0100

    Add personal statement section
    Add personal statement section

--- home.php
+++ home.php
@@ -68,31 +68,31 @@
 <!-- Navbar -->
 <div class="w3-top">
  <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
-  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Home</a>
-  <div class="w3-dropdown-hover w3-hide-small">
-    <button class="w3-button w3-padding-large" title="Account Settings"><i class="fa fa-user w3-margin-right"></i>Settings</button>
-    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
-      <a href="#" class="w3-bar-item w3-button">Edit Profile</a>
-      <a href="logout.php" class="w3-bar-item w3-button">Log out</a>
-    </div>
-  </div>
+	<a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Home</a>
+	<div class="w3-dropdown-hover w3-hide-small">
+		<button class="w3-button w3-padding-large" title="Account Settings"><i class="fa fa-user w3-margin-right"></i>Settings</button>
+		<div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
+			<a href="#" class="w3-bar-item w3-button">Edit Profile</a>
+			<a href="logout.php" class="w3-bar-item w3-button">Log out</a>
+		</div>
+	</div>
  </div>
 </div>
 
 <!-- Page Container -->
-<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
-  <!-- The Grid -->
-  <div class="w3-row">
-    <!-- Left Column -->
-    <div class="w3-col m3">
-      <!-- Profile -->
-      <div class="w3-card w3-round w3-white">
-        <div class="w3-container">
-         <h4 class="w3-center"><?php echo $user->username; ?></h4>
-         <p class="w3-center"><img src="/w3images/avatar3.png" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
-         <hr>
+<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">
+	<!-- The Grid -->
+	<div class="w3-row">
+		<!-- Left Column -->
+		<div class="w3-col m3">
+			<!-- Profile -->
+			<div class="w3-card w3-round w3-white">
+				<div class="w3-container">
+				 <h4 class="w3-center"><?php echo $user->username; ?></h4>
+				 <p class="w3-center"><img src="/w3images/avatar3.png" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
+				 <hr>
 				 <?php
-						if($user_type == 'contractor') 
+						if($user_type == 'contractor')
 						{
 							echo '<p><i class="fa fa-address-book fa-fw w3-margin-right w3-text-theme"></i>' . $profile->first_name . ' ' . $profile->last_name . '</p>';
 							echo '<p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i>' . date("d/m/Y", strtotime($profile->date_of_birth)) . '</p>';
@@ -106,10 +106,10 @@
 						{
 							echo '<p><i class="fa fa-address-book fa-fw w3-margin-right w3-text-theme"></i> ' . $profile->company_name . '</p>';
 						}
-			   ?>
-        </div>
-      </div>
-      <br>
+				 ?>
+				</div>
+			</div>
+			<br>
 
 			<?php if($user_type == 'contractor') : ?>
 				<?php if($profile->skills) : ?>
@@ -127,34 +127,28 @@
 				<br>
 				<?php endif; ?>
 			<?php endif; ?>
-    </div>
+		</div>
 
-    <!-- Middle Column -->
-    <div class="w3-col m7">
-      
-      <div class="w3-container w3-card w3-white w3-round w3-margin work-experience-container">
-				<h3 class="work-experience-title">Work Experience</h3>
-				<hr>
-        <h5>YMCA North Tyneside</h5>
-        <p class="work-experience-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p class="work-experience-description">Donec semper neque vel hendrerit placerat. Nullam gravida odio ut lacus interdum, nec ullamcorper augue sollicitudin. Phasellus eget eleifend purus, ullamcorper cursus tortor. Aliquam eget ornare risus. Proin sed mauris sed enim lacinia semper et eu tortor. Nunc mattis aliquet venenatis. Nunc varius maximus suscipit. Aenean lobortis arcu dignissim urna efficitur vehicula et a lacus. Suspendisse dui elit, blandit vitae mollis id, volutpat at enim.</p>
-				
-        <h5>Cell Pack Solutions</h5>
-        <p class="work-experience-description">Nam venenatis massa in lectus gravida, sed dictum nibh maximus. Nunc quam enim, scelerisque eu risus blandit, suscipit iaculis nulla. Suspendisse interdum, sapien at efficitur lobortis, velit ex euismod libero, id varius quam purus vel odio. Sed in lobortis metus. Donec non eros neque. Integer id quam turpis. Quisque dignissim pharetra dapibus. Maecenas tincidunt eu nisi vitae cursus. In ultrices tempor urna, ac viverra mauris pretium eget.</p>
-        <p class="work-experience-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<!-- Middle Column -->
+		<div class="w3-col m7">
+
+			<div class="w3-container w3-card w3-white w3-round w3-margin work-experience-container">
+				<h3 class="work-experience-title">Personal Statement</h3>
+				<?php echo nl2br($profile->bio); ?>
 				<br>
-      </div> 
+				<br>
+			</div>
 
-    <!-- End Middle Column -->
-    </div>
+		<!-- End Middle Column -->
+		</div>
 
-    <div class="w3-col m2">
+		<div class="w3-col m2">
 
 		<?php
 
 			$user_sql   = "SELECT * FROM jobs;";
 			$user_query = mysqli_query($login_connect, $user_sql);
-		
+
 			while($row = mysqli_fetch_array($user_query))
 			{
 				?>
@@ -174,14 +168,14 @@
 			}
 
 		?>
-			
-    <!-- End Right Column -->
-    </div>
-    
-  <!-- End Grid -->
-  </div>
-  
+
+		<!-- End Right Column -->
+		</div>
+
+	<!-- End Grid -->
+	</div>
+
 <!-- End Page Container -->
 </div>
 </body>
-</html> 
+</html>
